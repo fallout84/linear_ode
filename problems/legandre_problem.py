ca3dabf48aeee7a6bc919c7bd7ebc65cb6f1d4d8
Максим Антонов <112693839+fallout84@users.noreply.github.com>
2026-04-23 16:46:35 +0300

    Update legandre_problem.py

--- problems/legandre_problem.py
+++ problems/legandre_problem.py
@@ -1,7 +1,12 @@
-@dataclass
-class BoundaryCondition:
-    # -1 - дирихле, 1 - нейман
-    left_type: int = 0.0
-    right_type: int = 0.0
-    left_val: float = 0.0
-    right_val: float = 0.0
+class Legandre(ODE):  
+    def __init__(self, n: int):
+        if n % 2 == 1:
+            bc = BoundaryCondition(left_type=-1, right_type=-1, left_val=0, right_val=1)
+        else:
+            bc = BoundaryCondition(left_type=1, right_type=-1, left_val=0, right_val=1)
+
+        super().__init__(
+            p=lambda x: -2 * x / (1 - x ** 2),
+            q=lambda x: n * (n + 1) / (1 - x ** 2),
+            f=lambda x: 0,
+            bc=bc)
