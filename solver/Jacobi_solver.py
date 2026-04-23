3c2598538b14bf778a176ba12506868680dd8c17
Максим Антонов <112693839+fallout84@users.noreply.github.com>
2026-04-23 16:33:49 +0300

    Update Jacobi_solver.py

---
+++ solver/Jacobi_solver.py
@@ -0,0 +1,15 @@
+class Jacobi(LinearSolver):
+    def __init__(self, f: float = 0.01, tol: float = 1e-5, max_iter: int = 1000000):
+        super().__init__(tol, max_iter)
+        self.f = f
+
+    def solve(self, a_matrix: np.ndarray, b: np.ndarray, fix: float = 0.01) -> bool:
+        tau = np.diag(a_matrix)
+        y = np.zeros(len(b))
+        for _ in range(self.max_iter):
+            r = b - a_matrix @ y
+            if np.linalg.norm(r) < self.tol:
+                self.y = y
+                return True
+            y += r * fix / tau
+        return False
